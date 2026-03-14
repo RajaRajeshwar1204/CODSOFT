4bc3e64e45e3491c74c0eb7aa5301f617d4a7243
BATHINI RAJA RAJESHWAR <rajarajeshwargoudbatthini@gmail.com>
2026-03-14 07:41:56 +0530

    Update spam_sms.py

--- spam_sms.py
+++ spam_sms.py
@@ -4,38 +4,22 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report
 
-# ===============================
-# 1. Load Dataset
-# ===============================
-
 data = pd.read_csv("C:/Users/edlap/Documents/FInal year Project/Raja/Codsoft/spam.csv", encoding="latin-1")
-# Keep only needed columns
 data = data[['v1', 'v2']]
 data.columns = ['label', 'message']
 
-# Convert labels to numbers
 data['label'] = data['label'].map({'ham':0, 'spam':1})
-
-# ===============================
-# 2. Train/Test Split
-# ===============================
 
 X_train, X_test, y_train, y_test = train_test_split(
     data['message'], data['label'], test_size=0.2, random_state=42
 )
 
-# ===============================
-# 3. TF-IDF Feature Extraction
-# ===============================
 
 vectorizer = TfidfVectorizer(stop_words='english')
 
 X_train_tfidf = vectorizer.fit_transform(X_train)
 X_test_tfidf = vectorizer.transform(X_test)
 
-# ===============================
-# 4. Train Model
-# ===============================
 
 model = LogisticRegression()
 
@@ -51,9 +35,6 @@ print("Accuracy:", accuracy_score(y_test, predictions))
 print("\nClassification Report:\n")
 print(classification_report(y_test, predictions))
 
-# ===============================
-# 6. Test With Custom Message
-# ===============================
 
 def predict_sms(text):
 
